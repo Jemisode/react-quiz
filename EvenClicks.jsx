990c6380335d39a2a781ea3327b6be3cf9ef15ea
Jess <jessmatthews@live.co.uk>
2019-09-06 11:23:28 +0100

    created an EvenClicks component that displays Even if has been clicked an even number of times and Odd if it has been clicked an odd number of times

---
+++ EvenClicks.jsx
@@ -0,0 +1,41 @@
+// Create a component <EvenClicks> that displays "Even" if it's been clicked an 
+// even number of times and "Odd" if it's been clicked an odd number of times.
+
+import React, { Component } from "react";
+
+class EvenClicks extends Component {
+	// add a constructor method
+	// it gets passed props as its first argument
+	constructor(props) {
+		// make sure you always add this, it makes Component work
+		super(props);
+
+		// set up our state
+		// just a plain old JavaScript object
+		this.state = {
+			clicked: false,
+		};
+
+		// force this to always be *this* this in handleClick
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		// set new value
+		// pass in a JavaScript object with values we want to updated
+		this.setState({ clicked: true })
+	}
+
+	render() {
+		return (
+			<header className="jumbotron" onClick= { this.handleClick }>
+				<p onClick={ this.handleClick }>
+					{this.state.clicked ? "Even" : "Odd" }				
+				</p>
+			</header>
+		);
+	}
+}
+
+export default EvenClicks;
+
